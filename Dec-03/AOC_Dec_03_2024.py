dc69725feb097164979341df837fb50e96dc647d
Michael Hibbard <mikejhibbard@gmail.com>
2024-12-03 08:36:37 +0000

    Dec 3rd - set up

---
+++ Dec-03/AOC_Dec_03_2024.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+test_data = """xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"""
+
+# Test Result = 161 (2*4 + 5*5 + 11*8 + 8*5)
+
+# Read and extract input data
+path = Path('input.txt')
+
+# Parse into a list of lines
+# lines = path.read_text().splitlines()
+lines = test_data.splitlines()
+
+
+"""PART 1"""
+
+
+
+# Read and extract input data
+path = Path('input.txt')
+
+# Parse into a list of lines
+# lines = path.read_text().splitlines()
+lines = test_data.splitlines()
+
+# Test - print first line
+# print(lines[0])
+
+
+
+
+print("\nPART 1 ANSWER")
+print("--------------")
+print(f"Answer: {None}")
+print("--------------")
+
+
+
+
+print("\nPART 2 ANSWER")
+print("--------------")
+print(f"Answer: {None}")
+print("--------------")

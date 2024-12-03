b5ec255317cc8495e4699294f568c5029b375e8d
Michael Hibbard <mikejhibbard@gmail.com>
2024-12-03 08:38:27 +0000

    Template fixes

--- Dec-XX/AOC_Dec_XX_2024.py
+++ Dec-XX/AOC_Dec_XX_2024.py
@@ -3,28 +3,18 @@ from pathlib import Path
 test_data = """
 """
 
-# Read and extract input data
-path = Path('input.txt')
-
-# Parse into a list of lines
-# lines = path.read_text().splitlines()
-lines = test_data.splitlines()
-
-
-"""PART 1"""
-
-
+#  Test Result:
 
 # Read and extract input data
 path = Path('input.txt')
 
+
 # Parse into a list of lines
 # lines = path.read_text().splitlines()
 lines = test_data.splitlines()
-
-# Test - print first line
 # print(lines[0])
 
+"""PART 1"""
 
 
 
@@ -32,7 +22,6 @@ print("\nPART 1 ANSWER")
 print("--------------")
 print(f"Answer: {None}")
 print("--------------")
-
 
 
 

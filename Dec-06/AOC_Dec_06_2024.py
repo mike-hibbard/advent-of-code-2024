51dda9542aebaf16cdba626708eefe6a825a698c
Michael Hibbard <mikejhibbard@gmail.com>
2024-12-11 17:59:37 +0000

    Dec 6th - initial work

--- Dec-06/AOC_Dec_06_2024.py
+++ Dec-06/AOC_Dec_06_2024.py
@@ -1,6 +1,15 @@
 from pathlib import Path
 
-test_data = """
+test_data = """....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
 """
 
 #  Test Result:
@@ -16,6 +25,40 @@ lines = test_data.splitlines()
 
 """PART 1"""
 
+# Split input text into an array (numpy?)
+grid =[]
+for line in lines:
+    grid.append(list(line))
+
+def print_grid(grid):
+    for row in grid:
+        print(row)
+
+print_grid(grid)
+
+# Get start point
+# Get direction
+# Start walking
+# Identify blocks
+# Logic to turn right
+# Logic to write X in prior space after every move
+
+def find_start_position(grid):
+    
+    start_row = 0
+    start_col = 0
+
+    for i, row in enumerate(grid):
+        if '^' in row:
+            start_row = i
+
+    for j, col in enumerate(grid[start_row]):
+        if col == '^':
+            start_col = j
+
+    return (start_row, start_col)
+
+print(f"start position: {find_start_position(grid)}")
 
 
 print("\nPART 1 ANSWER")
